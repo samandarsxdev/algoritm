27eb04fd500bb0875e1e6ad8332b3e8ef35cafa4
samandarsxdev <93664571+samandarsxdev@users.noreply.github.com>
2021-11-28 02:00:35 -0500

    add

--- main.js
+++ main.js
@@ -1,5 +1,82 @@
- var arry = [1,2,3,4,0,true,false,undefined,null]
+// let key = ["salom","dunyo"];
+// let board = ["Salom"]
 
-for(var i = 0; i<=arry.length;i++){
+// console.log(key);
+// console.log(get(key,board));
+
+// function get (key,board){
+ 
+//     for(let item of key){
+//         if(key.tolowerCase() === board.tolowerCase()){
+//         return true;
+//         }
+
+//     }
+//  return false//   return true
+// }
+
+// let age1=prompt("birinchi son")-0
+// let age2=prompt("ikkinchi son")-0
+// let age3 = prompt("uchinchi son")-0
+// if(Math.max(age1,age2,age3)){
+//     console.log(Math.max(age1,age2,age3) , Math.min(age1,age2,age3));
+// }else{
+//     console.log();
+// }
+
+// let x = prompt("x ga qiymat bering")-0;
+// if(x>0){
+//     let a = 2*Math.sin(x);
+//     console.log(a);
+// }else if(x<=0){
+//     let b = x-6;
+//     console.log(b);
+// }
+
+// let x = prompt("x ga son kiriting")
+
+// if (x<0) {
+//     console.log(`${x} soni manfiy son`);
     
+// }else if(x==0){
+//     console.log(`${x} soni nolga teng`);
+// }else{
+//     console.log(`${x} soni musbat son`);
+// }
+
+// let k = prompt("son kiriting");
+// for(let n=0;n<=k; n++){
+//     console.log(n);
+// }
+
+// let a = prompt("a sonini kiriting")
+// let b = prompt("b sonini kiriting")
+//  if (a>b) {
+//      console.log("a soni b sondan kichkina");
+//  }
+//  for(let i=a;i<=b;i++){
+//      console.log(i);
+//  }
+
+//  let narx = prompt("narxni kiriting")
+//  for(let i=0.1;i<=1;i=i+0.1){
+//      console.log(i*narx);
+//
+
+// let a= prompt("a ni elon qiling")-0;
+// let b= prompt("b ni elon qiling")-0;
+// if (a>b) {
+//     console.log("a soni b sonidan katta");
+// }
+// for(let i=a;i<=b;i++){
+//     console.log(a+=i);
+// }
+
+
+let x = prompt("son kiriting!!!")
+for(let i = 0; i<=n ; i++){
+    if () {
+        
+    }
 }
+
